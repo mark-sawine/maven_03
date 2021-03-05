687ca3517a11ce41093abe36be1962c0ac7d592b
mark-sawine <80096791+mark-sawine@users.noreply.github.com>
2021-03-05 22:40:05 +0100

    commento

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hell GitHub!");
+        System.out.println("Hello GitHub!");
+
+        //modify from IntelliJ
     }
 }
